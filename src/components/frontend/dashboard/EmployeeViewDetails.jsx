1471393fa3143eab1f3ab2cce4d0f104f14a0bf1
ripon <riponahmed2201@gmail.com>
2023-06-26 14:55:38 +0600

    employee view page and other issue ok

--- src/components/frontend/dashboard/EmployeeViewDetails.jsx
+++ src/components/frontend/dashboard/EmployeeViewDetails.jsx
@@ -1,7 +1,10 @@
 import axios from "axios";
 import React, { useCallback, useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { token } from "../../../utils/authentication";
+import Loader from "../../loadar/Loader";
+import defaultImage from "../../../assets/images/default.png";
+import moment from "moment";
 
 function EmployeeViewDetails() {
   const { id } = useParams();
@@ -38,252 +41,231 @@ function EmployeeViewDetails() {
   return (
     <section className="SelectedEmployee">
       <div className="container">
-        <div className="row">
-          <div className="col-xl-4 col-md-5">
-            <div className="row">
-              <div className="col-xl-6">
-                <div className="selectedEmploImgBackBtn">
-                  <a href>
-                    <img
-                      src="/assets/frontend/images/selectedEmployee/backButton.png"
-                      className="img-fluid"
-                      alt="image"
-                    />
-                  </a>
-                </div>
-              </div>
-              <div className="col-xl-6">
-                <div className="selectedEmployeeBookmarkImg">
-                  <a href>
-                    <img
-                      src="/assets/frontend/images/selectedEmployee/SelecEmployeeCardBookmaricon.png"
-                      className="img-fluid"
-                      alt="image"
-                    />
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div className="row">
-              <div className="selectedEmpProfilePic">
-                <img
-                  src="/assets/frontend/images/selectedEmployee/slcEmpPro.png"
-                  className="img-fluid"
-                  alt="image"
-                />
-              </div>
-            </div>
-            <div className="row">
-              <div className="profileMarker">
-                <img
-                  src="/assets/frontend/images/selectedEmployee/profileMarkLogo.png"
-                  className="img-fluid"
-                  alt="image"
-                />
-              </div>
-            </div>
-            <div className="row">
-              <div
-                className="selectProfileRowForMargin"
-                style={{ width: "auto", borderRadius: 14 }}
-              >
-                <div className="card selectEmpCardCard_body card-body">
-                  <div className="sctEmplTitleBox">
-                    <h5 className="card-title slctProfileNameTitle">
-                      Mr Alquraish Sharkar
-                    </h5>
-                    <span className="sctProfileAge">Age: 45 years</span>
-                    <span className="slctProfiletotalRating">
-                      <img
-                        src="/assets/frontend/images/selectedEmployee/Star 1.png "
-                        className="img-fluid scltEmpProfileRatingIcon"
-                        alt="image"
-                      />
-                      4.5 (15)
-                    </span>
-                    <div className="slctproLine">
-                      <img
-                        src="/assets/frontend/images/selectedEmployee/Line 1.png"
-                        className="img-fluid"
-                        alt="image"
-                      />
-                    </div>
-                  </div>
-                  <div className="row slctEmpProfileRateRow">
-                    <div className="col-xl-7 col-md-6">
-                      <span className="slctProfileRateName">
-                        <img
-                          className="img-fluid SlctProrateIcon"
-                          src="/assets/frontend/images/selectedEmployee/rate.png"
-                          alt="image"
-                        />
-                        Rate:
-                      </span>
-                      <span className="slctprofilePerHourPrice">500£/Hour</span>
-                    </div>
-                    <div className="col-xl-5 col-md-6">
-                      <span className="slctProfileExpName">
-                        <img
-                          src="/assets/frontend/images/selectedEmployee/experience.png"
-                          alt="image"
-                        />
-                        Exp:
-                      </span>
-                      <span className="slctProfileExpValue">5 years</span>
-                    </div>
-                  </div>
-                  <div className="row slctEmpProfileRateRow">
-                    <div className="col-xl-7 col-md-6">
-                      <span className="selectEmployeeTotalHours">
-                        <img
-                          src="/assets/frontend/images/selectedEmployee/clock.png"
-                          alt="image"
-                        />
-                        Total Hours:
-                      </span>
-                      <span className="selectEmployeeTotalHoursValue">
-                        2456 H
-                      </span>
-                    </div>
-                    <div className="col-xl-5 col-md-6 selectEmpReviewCol">
-                      <span className="selectEmpReviewspan">
-                        <img
-                          src="/assets/frontend/images/selectedEmployee/Review.png"
-                          alt="image"
-                        />
-                        Review:
-                      </span>
-                      <span className="selectEmpReviewspanValue">1 time</span>
-                    </div>
-                  </div>
-                  <div className="row slcEmpProfilLicenseRow">
-                    <div className="col-xl-12">
-                      <span className="selectEmpLicense">
-                        <img
-                          src="/assets/frontend/images/selectedEmployee//licenceLogo.png"
-                          className="img-fluid selectEmpLicenseLogo"
-                          alt="image"
-                        />
-                        License No:
-                      </span>
-                      <span className="selectEmpLicenseValue">
-                        MN123456-12345
-                      </span>
-                    </div>
-                  </div>
-                  <div className="selectEmployeeCardBookNowButton">
-                    <button>Book Now</button>
-                  </div>
-                </div>
-              </div>
-            </div>
+        {loading ? (
+          <div className="card-body">
+            <Loader /> <br />
+            <br />
+            <br />
           </div>
-          <div className="col-xl-8 col-md-7">
-            <div className="selectEmpLocationBar">
-              <p>Location</p>
-              <img
-                src="/assets/frontend/images/selectedEmployee/Line 2.png"
-                className="img-fluid"
-                alt="image"
-              />
-              <span>
-                <img
-                  src="/assets/frontend/images/selectedEmployee/location.png"
-                  alt="image"
-                />
-                &nbsp;20 Ranipukur Lane, Ciatle, #PO 42345, Camodia
-              </span>
-            </div>
-            <div className="selectEmpLocationBar">
-              <p>Education</p>
-              <img
-                src="/assets/frontend/images/selectedEmployee/Line 2.png"
-                className="img-fluid"
-                alt="image"
-              />
-              <span>
-                <img
-                  src="/assets/frontend/images/selectedEmployee/education (2).png"
-                  alt="image"
-                />
-                &nbsp;2015-17: Oxford High School, Camodia
-              </span>
+        ) : (
+          <div className="row">
+            <div className="col-xl-4 col-md-5">
               <div className="row">
+                <div className="col-xl-6">
+                  <div className="selectedEmploImgBackBtn">
+                    <Link to="/client-dashboard">
+                      <img
+                        src="/assets/frontend/images/selectedEmployee/backButton.png"
+                        className="img-fluid"
+                        alt="alt-image"
+                      />
+                    </Link>
+                  </div>
+                </div>
+                <div className="col-xl-6">
+                  <div className="selectedEmployeeBookmarkImg">
+                    <a href>
+                      <img
+                        src="/assets/frontend/images/selectedEmployee/SelecEmployeeCardBookmaricon.png"
+                        className="img-fluid"
+                        alt="alt-image"
+                      />
+                    </a>
+                  </div>
+                </div>
+              </div>
+              <div className="row">
+                <div className="selectedEmpProfilePic">
+                  <img
+                    src={
+                      getSingleEmployeeDetails?.profilePicture
+                        ? process.env.REACT_APP_ASSETs_BASE_URL +
+                          "/" +
+                          getSingleEmployeeDetails?.profilePicture
+                        : defaultImage
+                    }
+                    className="img-fluid"
+                    alt="alt-image"
+                  />
+                </div>
+              </div>
+              <div className="row">
+                <div className="profileMarker">
+                  <img
+                    src="/assets/frontend/images/selectedEmployee/profileMarkLogo.png"
+                    className="img-fluid"
+                    alt="alt-image"
+                  />
+                </div>
+              </div>
+              <div className="row">
+                <div
+                  className="selectProfileRowForMargin"
+                  style={{ width: "auto", borderRadius: 14 }}
+                >
+                  <div className="card selectEmpCardCard_body card-body">
+                    <div className="sctEmplTitleBox">
+                      <h5 className="card-title slctProfileNameTitle">
+                        {getSingleEmployeeDetails?.name}
+                      </h5>
+                      <span className="sctProfileAge">
+                        Date Of Birth:
+                        {moment(getSingleEmployeeDetails?.dateOfBirth)
+                          .format("YYYY-MM-DD")
+                          .valueOf()}
+                      </span>
+                      <span className="slctProfiletotalRating">
+                        <img
+                          src="/assets/frontend/images/selectedEmployee/Star 1.png "
+                          className="img-fluid scltEmpProfileRatingIcon"
+                          alt="alt-image"
+                        />
+                        {getSingleEmployeeDetails?.rating}
+                      </span>
+                      <div className="slctproLine">
+                        <img
+                          src="/assets/frontend/images/selectedEmployee/Line 1.png"
+                          className="img-fluid"
+                          alt="alt-image"
+                        />
+                      </div>
+                    </div>
+                    <div className="row slctEmpProfileRateRow">
+                      <div className="col-xl-7 col-md-6">
+                        <span className="slctProfileRateName">
+                          <img
+                            className="img-fluid SlctProrateIcon"
+                            src="/assets/frontend/images/selectedEmployee/rate.png"
+                            alt="alt-image"
+                          />
+                          Rate:
+                        </span>
+                        <span className="slctprofilePerHourPrice">
+                          {getSingleEmployeeDetails?.hourlyRate}£/Hour
+                        </span>
+                      </div>
+                      <div className="col-xl-5 col-md-6">
+                        <span className="slctProfileExpName">
+                          <img
+                            src="/assets/frontend/images/selectedEmployee/experience.png"
+                            alt="alt-image"
+                          />
+                          Exp:
+                        </span>
+                        <span className="slctProfileExpValue">
+                          {getSingleEmployeeDetails?.employeeExperience}
+                        </span>
+                      </div>
+                    </div>
+                    <div className="row slctEmpProfileRateRow">
+                      <div className="col-xl-7 col-md-6">
+                        <span className="selectEmployeeTotalHours">
+                          <img
+                            src="/assets/frontend/images/selectedEmployee/clock.png"
+                            alt="alt-image"
+                          />
+                          Total Hours:
+                        </span>
+                        <span className="selectEmployeeTotalHoursValue">
+                          {getSingleEmployeeDetails?.totalWorkingHour} H
+                        </span>
+                      </div>
+                      <div className="col-xl-5 col-md-6 selectEmpReviewCol">
+                        <span className="selectEmpReviewspan">
+                          <img
+                            src="/assets/frontend/images/selectedEmployee/Review.png"
+                            alt="alt-image"
+                          />
+                          Review:
+                        </span>
+                        <span className="selectEmpReviewspanValue">1 time</span>
+                      </div>
+                    </div>
+                    <div className="row slcEmpProfilLicenseRow">
+                      <div className="col-xl-12">
+                        <span className="selectEmpLicense">
+                          <img
+                            src="/assets/frontend/images/selectedEmployee//licenceLogo.png"
+                            className="img-fluid selectEmpLicenseLogo"
+                            alt="alt-image"
+                          />
+                          License No:
+                        </span>
+                        <span className="selectEmpLicenseValue">
+                          {getSingleEmployeeDetails?.licensesNo}
+                        </span>
+                      </div>
+                    </div>
+                    {/* <div className="selectEmployeeCardBookNowButton">
+                      <button>Book Now</button>
+                    </div> */}
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-xl-8 col-md-7">
+              <div className="selectEmpLocationBar">
+                <p>Location</p>
+                <img
+                  src="/assets/frontend/images/selectedEmployee/Line 2.png"
+                  className="img-fluid"
+                  alt="alt-image"
+                />
+                <span>
+                  <img
+                    src="/assets/frontend/images/selectedEmployee/location.png"
+                    alt="alt-image"
+                  />
+                  &nbsp; {getSingleEmployeeDetails?.presentAddress}
+                </span>
+              </div>
+              <div className="selectEmpLocationBar">
+                <p>Education</p>
+                <img
+                  src="/assets/frontend/images/selectedEmployee/Line 2.png"
+                  className="img-fluid"
+                  alt="alt-image"
+                />
                 <span>
                   <img
                     src="/assets/frontend/images/selectedEmployee/education (2).png"
-                    alt="image"
+                    alt="alt-image"
                   />
-                  &nbsp;2018-20: (B.Sc.)Cambridge University, Camodia
+                  &nbsp;{getSingleEmployeeDetails?.higherEducation}
                 </span>
               </div>
-            </div>
-            <div className="selectEmpLocationBar">
-              <p>Certificate</p>
-              <img
-                src="/assets/frontend/images/selectedEmployee/Line 2.png"
-                className="img-fluid"
-                alt="image"
-              />
-              <span>
-                <img
-                  src="/assets/frontend/images/selectedEmployee/certificateICon.png"
-                  alt="image"
-                />
-                &nbsp;College Certificate in Culinary Skills
-              </span>
-              <div className="row">
-                <span>
-                  <img
-                    src="/assets/frontend/images/selectedEmployee/certificateICon.png"
-                    alt="image"
-                  />
-                  &nbsp;College Certificate in Culinary Skills &amp; Cookery
-                </span>
+              <div className="selectEmpLocationBar">
+                <p>Certificate</p>
+                {getSingleEmployeeDetails?.certificates?.map((item, index) => (
+                  <div className="row" key={index}>
+                    <span>
+                      <img
+                        src="/assets/frontend/images/selectedEmployee/certificateICon.png"
+                        alt="custom-image"
+                      />
+                      &nbsp; {item?.certificateName}
+                    </span>
+                  </div>
+                ))}
               </div>
-              <span>
-                <img
-                  src="/assets/frontend/images/selectedEmployee/certificateICon.png"
-                  alt="image"
-                />
-                &nbsp;Advanced Certificate in Culinary Arts
-              </span>
-              <div className="row">
-                <span>
-                  <img
-                    src="/assets/frontend/images/selectedEmployee/certificateICon.png"
-                    alt="image"
-                  />
-                  &nbsp;Certificate IV Program
-                </span>
-              </div>
-            </div>
-            <div className="selectEmpLocationBar">
-              <p>Language</p>
-              <img
-                src="/assets/frontend/images/selectedEmployee/Line 2.png"
-                className="img-fluid"
-                alt="image"
-              />
-              <span>
-                <img
-                  src="/assets/frontend/images/selectedEmployee/googleTranslate.png"
-                  alt="image"
-                />
-                &nbsp;English (Native)
-              </span>
-              <div className="row">
-                <span>
-                  <img
-                    src="/assets/frontend/images/selectedEmployee/googleTranslate.png"
-                    alt="image"
-                  />
-                  &nbsp;Spanish, Hindi (Conversational)
-                </span>
+              <div className="selectEmpLocationBar">
+                <p>Language</p>
+
+                {getSingleEmployeeDetails?.languages?.map((item, index) => (
+                  <div className="row" key={index}>
+                    <span>
+                      <img
+                        src="/assets/frontend/images/selectedEmployee/googleTranslate.png"
+                        alt="custom-image"
+                      />
+                      &nbsp;{item}
+                    </span>
+                  </div>
+                ))}
               </div>
             </div>
           </div>
-        </div>
+        )}
       </div>
     </section>
   );
